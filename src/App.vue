b9203f3ea9a5b1a1d7df27f456210d497c068cac
YoussefHarizi <youssef.harizi@gmail.com>
2022-12-11 08:17:33 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+
+
+    <div class="container">
+      <Header title="Task Tracker"/>
+      <Tasks :tasks="tasks"/>
+    </div>
+  
+
+  
+</template>
+<script>
+import Header from './components/Header.vue'
+import Tasks from './components/Tasks.vue'
+export default {
+  name:'App',
+  components:{
+    Header,
+    Tasks
+
+  },
+  data(){
+    return {
+
+      tasks:[]
+    }
+  },
+  created(){
+    this.tasks=[
+      {
+        id:1,
+        text:'First Task',
+        day:'March 1st at 2:30pm',
+        remender:true,
+      },
+      {
+        id:2,
+        text:'second Task',
+        day:'Juin 1st at 2:30pm',
+        remender:false,
+      },
+      {
+        id:3,
+        text:'Therd Task',
+        day:'March 1st at 2:30pm',
+        remender:true,
+      },
+      ]
+  }
+}
+</script>
+
